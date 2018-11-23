43ad61ec3104c90fdfdfdf030498568c0fc4295a
daemonw0lf <41929991+daemonw0lf@users.noreply.github.com>
2018-11-22 21:05:00 -0800

    Update daemonw0lf_intel_generator.sh

--- daemonw0lf_intel_generator.sh
+++ daemonw0lf_intel_generator.sh
@@ -1,90 +1,170 @@
  #######################################################################################
  #             DaemonWolf Labs - Threat Intelligence Generator for Bro                 #
  #######################################################################################
- # PREREQUISITS
- # You must have Bro installed, and bro-otx (https://github.com/hosom/bro-otx) installed to "/opt/bro/share/bro/site".
- # You must have mal-dnssearch (https://github.com/jonschipp/mal-dnssearch) installed. *Easiest Method
- # or just copy either mal-dns2bro.sh|py script and path to it under each line below.
+ #  This script will install all dependancies that do not exist. All blocklists are downloaded, parsed and pre-formatted to be loaded into Bro's Intelligence Framework.
+ #
+ #  PREREQUISITS
+ # You must have Bro and mal-dnssearch installed (credit to jonschipp https://github.com/jonschipp/mal-dnssearch).
  # 
  # BEGIN
  #
+ # Dependancy Check for mal-dnssearch (credit to jonschipp)
+ if [ ! -d ~/mal-dnssearch ]; then
+   echo "DOES NOT EXIST"
+    cd ~/
+     git clone https://github.com/jonschipp/mal-dnssearch
+      cd ~/mal-dnssearch
+       make install
+        exit 1;
+ else
+   echo "Requirements = PASS"
+ fi
  # Download's the latest BBcan177 Malicious Domains_blocklist, then formats the file and exports the intel file to /var/tmp/bbcan177_domains_new.intel
  curl -s https://gist.githubusercontent.com/BBcan177/4a8bf37c131be4803cb2/raw | grep -v '^#' | sed '/^\s*$/d' > /var/tmp/bbcan177_domains_new.txt &&
  mal-dns2bro -T dns -f /var/tmp/bbcan177_domains_new.txt -s https://gist.githubusercontent.com/BBcan177/4a8bf37c131be4803cb2/raw -n true > /var/tmp/bbcan177.intel
-
+ if [ -e "/var/tmp/bbcan177.intel" ]; then
+ echo "bbcan177 list Ok"
+ else
+ echo "failed to download bbcan177"
+ fi
  # Download's the latest C&C Domains blocklist, then formats the file and exports the intel file to /var/tmp/cc_domains_new.intel
  curl -s http://osint.bambenekconsulting.com/feeds/c2-dommasterlist.txt | grep -v '^#' | sed 's/,.*//' | sed '/^\s*$/d' > /var/tmp/cc_domains_new.txt &&
  mal-dns2bro -T dns -f /var/tmp/cc_domains_new.txt -s http://osint.bambenekconsulting.com/feeds/c2-dommasterlist.txt -n true > /var/tmp/cc.intel
-
+if [ -e "/var/tmp/cc.intel" ]; then
+ echo "C&C Domains list Ok"
+ else
+ echo "failed to download C&C Domains"
+ fi
  # Download's the latest Immortal Domains blocklist, then formats the file and exports the intel file to /var/tmp/immortal_domains.intel
  curl -s http://mirror2.malwaredomains.com/files/immortal_domains.txt | grep -v '^#' | sed '/^\s*$/d' > /var/tmp/immortal_domains_new.txt &&
  mal-dns2bro -T dns -f /var/tmp/immortal_domains_new.txt -s http://mirror2.malwaredomains.com/files/immortal_domains.txt -n true > /var/tmp/immortal.intel
- 
+ if [ -e "/var/tmp/immortal.intel" ]; then
+ echo "immortal list Ok"
+ else
+ echo "failed to download immortal"
+ fi
  # Download's the latest Ransomware Tracker blocklist, then formats the file and exports the intel file to /var/tmp/ransomware_domains_new.txt
  curl -s https://ransomwaretracker.abuse.ch/downloads/RW_DOMBL.txt | grep -v '^#' | sed '/^\s*$/d' > /var/tmp/ransomware_domains_new.txt &&
  mal-dns2bro -T dns -f /var/tmp/ransomware_domains_new.txt -s https://ransomwaretracker.abuse.ch/downloads/RW_DOMBL.txt -n true > /var/tmp/ransomware.intel
- 
+ if [ -e "/var/tmp/ransomware.intel" ]; then
+ echo "ransomware list Ok"
+ else
+ echo "failed to download ransomware"
+ fi
  # Download's the latest Dshield Malicious Domains blocklist, then formats the file and exports the intel file to /var/tmp/dshield_domains_new.intel
  curl -s https://secure.dshield.org/feeds/suspiciousdomains_Low.txt | grep -v '^#' | sed '/Site/d' | sed '/^\s*$/d' > /var/tmp/dshield_domains_new.txt &&
  mal-dns2bro -T dns -f /var/tmp/dshield_domains_new.txt -s https://secure.dshield.org/feeds/suspiciousdomains_Low.txt -n true > /var/tmp/dshield.intel
- 
+ if [ -e "/var/tmp/dshield.intel" ]; then
+ echo "dshield list Ok"
+ else
+ echo "failed to download dshield"
+ fi
  # Download's the latest Zeus Malicious Domains blocklist, then formats the file and exports the intel file to /var/tmp/zeus_domains_new.intel
  curl -s https://zeustracker.abuse.ch/blocklist.php?download=baddomains | grep -v '^#' |sed '/^\s*$/d' | sed '/^\s*$/d' > /var/tmp/zeus_domains_new.txt &&
  mal-dns2bro -T dns -f /var/tmp/zeus_domains_new.txt -s https://zeustracker.abuse.ch/blocklist.php?download=baddomains -n true > /var/tmp/zeus.intel
- 
+ if [ -e "/var/tmp/zeus.intel" ]; then
+ echo "zeus list Ok"
+ else
+ echo "failed to download zeus"
+ fi
  # Download's the latest Binary Defence IP Banlist, then formats the file and exports the intel file to /var/tmp/binarydefence_ips_new.intel
  curl -s https://www.binarydefense.com/banlist.txt | grep -v '^#' |sed '/^\s*$/d' | sed '/^\s*$/d' > /var/tmp/binarydefence_ips_new.txt &&
  mal-dns2bro -T ip -f /var/tmp/binarydefence_ips_new.txt -s https://www.binarydefense.com/banlist.txt -n true > /var/tmp/binarydefence.intel
-
+if [ -e "/var/tmp/binarydefence.intel" ]; then
+ echo "binarydefence list Ok"
+ else
+ echo "failed to download binarydefence"
+ fi
  # Download's the latest Malc0de IP Banlist, then formats the file and exports the intel file to /var/tmp/malc0de_ip_new.intel
  curl -s http://malc0de.com/bl/IP_Blacklist.txt | grep -v '^#' | grep -v '^//' |sed '/^\s*$/d' | sed '/^\s*$/d' > /var/tmp/malc0de_ip_new.txt &&
  mal-dns2bro -T ip -f /var/tmp/malc0de_ip_new.txt -s http://malc0de.com/bl/IP_Blacklist.txt -n true > /var/tmp/malc0de.intel
-
+if [ -e "/var/tmp/malc0de.intel" ]; then
+ echo "malc0de list Ok"
+ else
+ echo "failed to download malc0de"
+ fi
  # Download's the latest MalwareDomain's IP Banlist, then formats the file and exports the intel file to /var/tmp/malwaredomains_iplist_new.intel
  curl -s https://panwdbl.appspot.com/lists/mdl.txt | grep -v '^#' |sed '/^\s*$/d' | sed '/^\s*$/d' > /var/tmp/malwaredomains_iplist_new.txt &&
  mal-dns2bro -T ip -f /var/tmp/malwaredomains_iplist_new.txt -s https://panwdbl.appspot.com/lists/mdl.txt -n true > /var/tmp/malwaredomains.intel
-
+if [ -e "/var/tmp/malwaredomains.intel" ]; then
+ echo "malwaredomains list Ok"
+ else
+ echo "failed to download malwaredomains"
+ fi
  # Download's the latest EmergingThreats IP Banlist, then formats the file and exports the intel file to /var/tmp/et_iplist_new.intel
  curl -s http://rules.emergingthreats.net/blockrules/compromised-ips.txt | grep -v '^#' |sed '/^\s*$/d' | sed '/^\s*$/d' > /var/tmp/et_iplist_new.txt &&
  mal-dns2bro -T ip -f /var/tmp/et_iplist_new.txt -s http://rules.emergingthreats.net/blockrules/compromised-ips.txt -n true > /var/tmp/et.intel
- 
+if [ -e "/var/tmp/et.intel" ]; then
+ echo "EmergingThreats list Ok"
+ else
+ echo "failed to download EmergingThreats"
+ fi
  # Download's the latest Bambenek Domain Banlist, then formats the file and exports the intel file to /var/tmp/bambenek_domains_new.intel
  curl -s http://osint.bambenekconsulting.com/feeds/c2-dommasterlist-high.txt | grep -v '^#' | cut -d, -f1 | sed '/^\s*$/d' | sed '/^\s*$/d' > /var/tmp/bambenek_domains_new.txt &&
  mal-dns2bro -T dns -f /var/tmp/bambenek_domains_new.txt -s http://osint.bambenekconsulting.com/feeds/c2-dommasterlist-high.txt -n true > /var/tmp/bambenek.intel
- 
+ if [ -e "/var/tmp/bambenek.intel" ]; then
+ echo "bambenek list Ok"
+ else
+ echo "failed to download bambenek"
+ fi
  # Download's the latest OpenPhish URL's, then formats the file and exports the intel file to /var/tmp/openphish_new.txt
- curl -s https://openphish.com/feed.txt | grep -v '^#' | sed '/^\s*$/d' > /var/tmp/openphish_new.txt &&  
+ curl -s https://openphish.com/feed.txt | grep -v '^#' | sed '/^\s*$/d' > /var/tmp/openphish_new.txt &&
  mal-dns2bro -T url -f /var/tmp/openphish_new.txt -s https://openphish.com/feed.txt -n true > /var/tmp/openphish.intel
- 
+ if [ -e "/var/tmp/openphish.intel" ]; then
+ echo "openphish list Ok"
+ else
+ echo "failed to download openphish"
+ fi
  # Download's the latest EasyList Adservers, then formats the file and exports the intel file to /var/tmp/easylist_new.txt
- curl -s https://raw.githubusercontent.com/easylist/easylist/master/easylist/easylist_adservers.txt | sed 's/\||//g' | cut -f1 -d"^" > /var/tmp/easylist_new.txt && 
+ curl -s https://raw.githubusercontent.com/easylist/easylist/master/easylist/easylist_adservers.txt | sed 's/\||//g' | cut -f1 -d"^" > /var/tmp/easylist_new.txt &&
  mal-dns2bro -T dns -f /var/tmp/easylist_new.txt -s https://raw.githubusercontent.com/easylist/easylist/master/easylist/easylist_adservers.txt -n true > /var/tmp/easylist.intel
- 
+ if [ -e "/var/tmp/easylist.intel" ]; then
+ echo "easylist list Ok"
+ else
+ echo "failed to download easylist"
+ fi
  # Download's the latest ResCure Domains, then formats the file and exports the intel file to /var/tmp/rescure_domains.txt
- curl -s https://rescure.fruxlabs.com/rescure_domain_blacklist.txt | sed -e '1,11d'  | grep -v '^#' | sed '/^\s*$/d' > /var/tmp/rescure_domains.txt && 
- mal-dns2bro -T dns -f /var/tmp/rescure_domains.txt -s https://rescure.fruxlabs.com/rescure_domain_blacklist.txt -n true > /var/tmp/rescure_domains.intel
- 
- # Move to bro load directory
- mv /var/tmp/bbcan177.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/cc.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/dshield.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/immortal.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/ransomware.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/zeus.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/binarydefence.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/malc0de.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/malwaredomains.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/et.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/bambenek.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/openphish.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/easylist.intel /opt/bro/share/bro/site/bro-otx/scripts
- mv /var/tmp/rescure_domains.intel /opt/bro/share/bro/site/bro-otx/scripts
- 
- cat > /opt/bro/share/bro/site/bro-otx/scripts/__load__.bro << EOF
+ curl -s https://rescure.fruxlabs.com/rescure_domain_blacklist.txt | sed -e '1,11d'  | grep -v '^#' | sed '/^\s*$/d' > /var/tmp/rescure_domains.txt &&
+mal-dns2bro -T dns -f /var/tmp/rescure_domains.txt -s https://rescure.fruxlabs.com/rescure_domain_blacklist.txt -n true > /var/tmp/rescure_domains.intel
+ if [ -e "/var/tmp/rescure_domains.intel" ]; then
+ echo "rescure list Ok"
+ else
+ echo "failed to download rescure"
+ fi
+
+# Check if destination directory exists
+if [ -d "/opt/bro/share/bro/site" ]; then
+ echo "Directory Check = PASS"
+ else
+ mkdir "/opt/bro/share/bro/site"
+ fi
+# Check if load file exists
+ if [ -e "/opt/bro/share/bro/site/__load__.bro" ]; then
+ echo "Load File = PASS"
+ else
+ touch "/opt/bro/share/bro/site/__load__.bro"
+ fi
+
+ # Move newly created .intel files to load directory
+ mv /var/tmp/bbcan177.intel /opt/bro/share/bro/site
+ mv /var/tmp/cc.intel /opt/bro/share/bro/site
+ mv /var/tmp/dshield.intel /opt/bro/share/bro/site
+ mv /var/tmp/immortal.intel /opt/bro/share/bro/site
+ mv /var/tmp/ransomware.intel /opt/bro/share/bro/site
+ mv /var/tmp/zeus.intel /opt/bro/share/bro/site/
+ mv /var/tmp/binarydefence.intel /opt/bro/share/bro/site
+ mv /var/tmp/malc0de.intel /opt/bro/share/bro/site
+ mv /var/tmp/malwaredomains.intel /opt/bro/share/bro
+ mv /var/tmp/et.intel /opt/bro/share/bro/site
+ mv /var/tmp/bambenek.intel /opt/bro/share/bro/site
+ mv /var/tmp/openphish.intel /opt/bro/share/bro/site
+ mv /var/tmp/easylist.intel /opt/bro/share/bro/site
+
+ # Replace the contents of __load__.bro with below
+ cat > /opt/bro/share/bro/site/__load__.bro << EOF
  @load frameworks/intel/seen
  @load frameworks/intel/do_notice
  @load frameworks/files/hash-all-files
-
  redef Intel::read_files += {
                 fmt("%s/otx.dat", @DIR),
 		fmt("%s/bbcan177.intel", @DIR),
@@ -97,9 +177,8 @@
 		fmt("%s/malc0de.intel", @DIR),
 		fmt("%s/malwaredomains.intel", @DIR),
 		fmt("%s/et.intel", @DIR),
-		fmt("%s/easylist.intel", @DIR),
 		fmt("%s/openphish.intel", @DIR),
-		fmt("%s/rescure_domains.intel", @DIR),
+		fmt("%s/easylist.intel", @DIR),
 		fmt("%s/bambenek.intel", @DIR)
  };
 EOF
